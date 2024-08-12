a3561598d901823055594b77487ff7fa51cf3656
Paulo Lascano <smithlascano1@gmail.com>
2024-08-12 10:18:19 +0800

    updated backend

--- app/routes/salesRoutes.js
+++ app/routes/salesRoutes.js
@@ -33,7 +33,8 @@ router.get('/sales-by-products', authenticateToken, async (req, res) => {
             FROM orders o
             JOIN products p ON o.product_id = p.product_id
             WHERE o.orderStatus != 'PENDING'
-            GROUP BY p.product_id, p.productName;
+            GROUP BY p.product_id, p.productName
+            ORDER BY total_sales DESC;
         `;
         db.query(salesByProductsQuery, (err, result) => {
             if (err) {
@@ -45,6 +46,57 @@ router.get('/sales-by-products', authenticateToken, async (req, res) => {
         });
     } catch (error) {
         console.error('Error fetching sales by products:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/sales-by-month', authenticateToken, async (req, res) => {
+    try {
+        const salesByMonthQuery = `
+            SELECT 
+                DATE_FORMAT(STR_TO_DATE(orderDatenTime, '%m-%d-%Y %h:%i %p'), '%Y-%M') AS month,
+                SUM(priceInTotal) AS sales_amount,
+                SUM(orderQuantity) AS total_sales
+            FROM orders
+            WHERE orderStatus != 'PENDING'
+            GROUP BY month
+            ORDER BY STR_TO_DATE(CONCAT('01-', SUBSTRING(month, 6)), '%d-%M-%Y');
+        `;
+        db.query(salesByMonthQuery, (err, result) => {
+            if (err) {
+                console.error('Error fetching sales by products:', err);
+                res.status(500).json({ message: 'Internal Server Error' });
+            } else {
+                res.status(200).json(result);
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching sales by month:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/orders-by-month', authenticateToken, async (req, res) => {
+    try {
+        const ordersByMonthQuery = `
+            SELECT p.productName, 
+            DATE_FORMAT(STR_TO_DATE(orderDatenTime, '%m-%d-%Y %h:%i %p'), '%M') AS orderMonth, 
+            COUNT(o.order_id) AS orderCount
+            FROM products p
+            LEFT JOIN orders o ON p.product_id = o.product_id
+            GROUP BY p.productName, orderMonth
+            ORDER BY orderCount DESC;
+        `;
+        db.query(ordersByMonthQuery, (err, result) => {
+            if (err) {
+                console.error('Error fetching orders by products:', err);
+                res.status(500).json({ message: 'Internal Server Error' });
+            } else {
+                res.status(200).json(result);
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching orders by month:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const supplierRoutes = require('./app/routes/supplierRoutes.js');
 const orderRoutes = require('./app/routes/orderRoutes.js');
 const orderToSupplierRoutes = require('./app/routes/orderToSupplierRoutes.js');
 const salesRoutes = require('./app/routes/salesRoutes.js');
+const inventoryRoutes = require('./app/routes/inventoryRoutes.js');
+const freshproductRoutes = require('./app/routes/freshproductRoutes.js');
+const dashboardRoutes = require('./app/routes/dashboardRoutes.js');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes, customerRoutes, productRoutes, supplierRoutes, orderRoutes, orderToSupplierRoutes, salesRoutes);
+app.use('/api', userRoutes, customerRoutes, productRoutes, supplierRoutes, orderRoutes, orderToSupplierRoutes, salesRoutes, inventoryRoutes, freshproductRoutes, dashboardRoutes );
 
 const PORT = process.env.PORT || 3001;
 
